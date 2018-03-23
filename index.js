bc984c7a99ef8d6099e540b4f7e2d2f3f0a70e4a
Kimberly Sampson <kbburk54@yahoo.com>
2018-03-23 23:35:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,61 @@
+
+function shout(string){
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string){
+
+var string;
+ 
+if (dog === "hungry") {
+  console.log("Refilling food bowl.")
+ 
+} else if (dog === "thirsty") {
+  console.log("Refilling water bowl.")
+ 
+} else if (dog === "playful") {
+  console.log("Playing tug-of-war.")
+ 
+} else if (dog === "cuddly") {
+  console.log("Snuggling.")
+ 
+} else { 
+  console.log("Reading newspaper.")
+}
+
+  === uppercase
+ 
+  return "I can/'t hear you!";
+}
+
+
+
+
+
+
+
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase // true
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase() === lowercas
+
+
+//function sayHiToGrandma(string){
+//  return string.toUpperCase("yes indeed!");
+//}
+
